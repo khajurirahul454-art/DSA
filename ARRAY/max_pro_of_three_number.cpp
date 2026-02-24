cfa7d34432d6e7ab2d11f7510e9de190aa256f30
Rahul RG <khajurirahul454@gmail.com>
2026-02-24 17:09:07 +0530

    code is updated

---
+++ ARRAY/max_pro_of_three_number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+/*
+problrm : maximum product of three numbers (leet code 628)
+level : easy
+
+approch : product of three number in sorted array
+          max product will be 
+          product1 = nums[0]*nums[1]*nums[n-1]  when array contains more than one negative values
+          -> above product is (smaller*smaller*larger)
+          product2 = nums[n-1]*nums[n-2]*nums[n-3]  when array contains larger positive values
+          -> above product is (larger*larger*larger)
+          and returning the max of avbove two products;
+*/
+
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+       int maxpro = 0;
+       int n = nums.size();
+        maxpro = max(nums[0]*nums[1]*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
+       return maxpro; 
+    }
+};
